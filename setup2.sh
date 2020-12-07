2893370f3d0a46e9ad27ba9243711882effb0411
Shanima Thahir <shanimakthahir7@gmail.com>
2020-12-07 07:12:55 +0000

    first commit

---
+++ setup2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+yum install httpd php php-mysql -y
+service httpd restart
+chkconfig httpd on
+
+wget https://wordpress.org/wordpress-4.7.18.zip
+unzip wordpress-4.7.18.zip
+cp -pr wordpress/* /var/www/html/
+cp -pr wp-config-sample.php wp-config.php
