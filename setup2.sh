a42750a23f48c078068c69eba4d1c0b4f407fa89
shanima333 <75494918+shanima333@users.noreply.github.com>
2020-12-07 16:02:08 +0530

    Update setup2.sh

--- setup2.sh
+++ setup2.sh
@@ -6,4 +6,5 @@ chkconfig httpd on
 wget https://wordpress.org/wordpress-4.7.18.zip
 unzip wordpress-4.7.18.zip
 cp -pr wordpress/* /var/www/html/
+cd /var/www/html/
 cp -pr wp-config-sample.php wp-config.php
